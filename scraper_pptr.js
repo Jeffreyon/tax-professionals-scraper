7990ba10d5fde3ae752f838efab81063e4ef1731
jeffrey <jeffreyon11@gmail.com>
2022-12-05 19:54:38 +0100

    scraper prototype finished

--- scraper_pptr.js
+++ scraper_pptr.js
@@ -1,17 +1,61 @@
+/**
+ * Scraping Boilerplate
+ */
+
+let puppeteer = require("puppeteer-core");
 let cheerio = require("cheerio");
-require("isomorphic-fetch");
+let browserConnectionUrl = require("./lib/browser.js")({
+    chromeFlags: ["--headless"],
+});
 let fs = require("fs");
 
 async function runScraper(url) {
+    // connect puppeteer to broser from chrome launcher
+    let browser = await puppeteer.connect({
+        browserWSEndpoint: await browserConnectionUrl,
+    });
+
     try {
-        let no_of_leads = process.argv[2];
+        // create new page
+        let page = await browser.newPage();
+
+        // set up page optimization and timeout
+        page.setRequestInterception(true);
+
+        page.on("request", (req) => {
+            if (
+                req.resourceType() == "image" ||
+                req.resourceType() == "stylesheet" ||
+                req.resourceType() == "font"
+            ) {
+                req.abort();
+            } else {
+                req.continue();
+            }
+        });
+
+        let no_of_leads = 20;
+
+        console.log("Starting scraper");
 
         for (let i = 1; i <= no_of_leads; i++) {
-            let response = await fetch(url);
+            // navigate to url
+            await page.goto(url, { timeout: 0 });
 
-            let markup = await response.text();
-            let $ = cheerio.load(markup);
+            // wait for page to load
+            let selector = "body";
 
+            await page.waitForSelector(selector, { timeout: 0 });
+
+            // get resulting html
+            let pageData = await page.evaluate(() => {
+                return document.documentElement.innerHTML;
+            });
+
+            // load the html as cheerio object
+            let $ = cheerio.load(pageData);
+
+            // scrape the business name, email and website
             let bizname_selector = "#left-area .main_title";
             let email_selector =
                 "#gd_output_location-2 > div > div.geodir_post_meta.list-group-item.list-group-item-action.geodir-field-email > a";
@@ -45,12 +89,16 @@ async function runScraper(url) {
             if (err) console.log("Saving Error:\n" + err);
             else console.log("\nLeads saved");
         });
+
+        await browser.close();
     } catch (error) {
         console.log("Scraper Error:\n" + error);
+        await browser.close();
     }
 }
 
 let leads = [];
+
 runScraper(
     "https://blacktaxprofessionals.com/taxprofessionals/smart-business-solutions/"
 );
